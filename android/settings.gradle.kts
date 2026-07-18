58108357be63caf4ead858e66aa31b075f71f16b
moussabahouzi-ui <moussabahouzi@gmail.com>
2026-07-18 23:42:35 +0100

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -27,46 +27,7 @@ plugins {
 }
 
 dependencyResolutionManagement {
-    // 💎 التعديل السحري هنا: السماح للإضافات بحقن مستودعاتها لتفادي التعارض وانهيار البناء
     repositoriesMode.set(RepositoriesMode.PRESERVE_SETTINGS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "mediahub"
-include(":app")pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        properties.getProperty("flutter.sdk")
-            ?: throw GradleException("flutter.sdk not set in local.properties")
-    }
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    // 💎 الحزام الأمني: فرض الإصدارات المستقرة من نقطة الصفر لمنع التعارض
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
