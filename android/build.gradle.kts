38d0bc6e8c351ab410fa54f9e04f2b58bb7ad1c2
moussabahouzi-ui <moussabahouzi@gmail.com>
2026-07-18 22:54:02 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,11 +4,12 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        // فرض إصدار كوتلن القوي والحديث هنا
+        // المحافظة على إصدار كوتلن المتوافق الذي أصلحناه سابقاً
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
     }
 }
 
 plugins {
-    alias(libs.plugins.android.application) apply false
+    // فرض استخدام إصدار مستقر وآمن من أدوات أندرويد لتجنب أزمة AGP 9+
+    id("com.android.application") version "8.5.0" apply false
 }
